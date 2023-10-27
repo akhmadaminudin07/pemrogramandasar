fc9270805884ff6ecb2e0cddff46a8a4723f3cf1
akhmadaminudin07 <149169689+akhmadaminudin07@users.noreply.github.com>
2023-10-27 19:56:19 +0700

    Create Latihan1_variabel.py

---
+++ Latihan1_variabel.py
@@ -0,0 +1,35 @@
+a = 6
+b = 4
+c = 2
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print("penyelesaian:",(a+b)*(a-c))
+
+
+a = 8
+b = 5
+c = 3
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print("penyelesaian:",(a*b)/(a+c))
+
+
+a = 8
+b = 5
+t = 7
+print("a:",a)
+print("b:",b)
+print("t:",t) 
+print("penyelesaian:",1/2*(a+b)*t) 
+
+
+r = 7
+print("r:",r)
+print("keliling lingkaran adalah:",2*3.14*7)
+
+
+r = 9
+print("r:",r)
+print("Luas lingkaran adalah:",3.14*9*9)

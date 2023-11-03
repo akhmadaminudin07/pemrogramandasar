ecf367e9e8ad7d9da684fb743a4c8c26fcb4d04c
akhmadaminudin07 <149169689+akhmadaminudin07@users.noreply.github.com>
2023-11-03 19:54:16 +0700

    Create Latihan2biodata.py

---
+++ Latihan2biodata.py
@@ -0,0 +1,14 @@
+#Nama Mahasiswa
+nama = input("Masukkan nama:")
+nim = input("Masukkan nim:")
+kelas = input("Masukkan kelas:")
+prodi = input("Masukkan prodi:")
+
+#cetak hasil
+print("-----------------")
+print("Biodata Mahasiswa")
+print("-----------------")
+print("nama:",nama)
+print("nim:",nim)
+print("kelas:",kelas)
+print("prodi:",prodi)
